1389be115c5d3f42cb61c920c146821b9765c986
Titas Chanda <titas.chanda@gmail.com>
2023-12-20 23:32:16 +0530

    documentation

--- docs/make.jl
+++ docs/make.jl
@@ -23,3 +23,14 @@ settings = Dict(
 makedocs(; sitename=sitename, settings...)
 
 ############################################################################
+
+if get(ENV, "GITHUB_EVENT_NAME", nothing) == "workflow_dispatch"
+  ENV["GITHUB_EVENT_NAME"] = "push"
+end
+
+deploydocs(;
+  repo="github.com/titaschanda/TeNLib.jl.git",
+  devbranch="main",
+  push_preview=true,
+  deploy_config=Documenter.GitHubActions(),
+)
