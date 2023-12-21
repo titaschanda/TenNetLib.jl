8c15c4f976c4105c94ec04dfd826e013aad11886
Titas Chanda <titas.chanda@gmail.com>
2023-12-21 06:25:42 +0530

    docstrings updated

--- src/mps/sweep.jl
+++ src/mps/sweep.jl
@@ -390,7 +390,7 @@ function krylov_extend!(psi::MPS, H::MPO; kwargs...)
     extension_applyH_cutoff::Float64 = get(kwargs, :extension_applyH_cutoff,
                                            Float64_threshold())
     extension_applyH_maxdim::Int = get(kwargs, :extension_applyH_maxdim,
-                                       maxlinkdim(sysenv.psi) + 1)
+                                       maxlinkdim(psi) + 1)
     extension_cutoff::Float64 = get(kwargs, :extension_cutoff,
                                     1E-10)
     
