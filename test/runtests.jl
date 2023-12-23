f48cad1fe55271aa2a3702505a43d84eaf46307b
Titas Chanda <titas.chanda@gmail.com>
2023-12-23 23:03:22 +0530

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using TeNLib
 using Test
 
 @testset "MPS" begin
-#    @time include("test_MPS_DMRG.jl")
-#    @time include("test_MPS_TDVP.jl")
+    @time include("test_MPS_DMRG.jl")
+    @time include("test_MPS_TDVP.jl")
 end
 
 
